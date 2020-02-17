fee9546cf73c889353ad3d0dfa632d475249701f
Zev Hoffman <zevubu@gmail.com>
2020-02-17 14:45:20 -0800

    built out hapi server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const Hapi = require("hapi");
+const routes = require("./routes");
+
+const app = async config => {
+    const{ host, port } = config;
+
+    // create an instance of hapi
+    const server = Hapi.server( {host, port });
+
+    // store the config for later use
+    server.app.config = config;
+
+    await routes.register( server );
+    return server;
+};
+
+module.exports = app;
